b4a962624464555daf2f6e551eafab3c7e280e6d
Mohamed Houssas <mhoussas26@gmail.com>
2025-02-15 21:13:37 +0100

    Tree

--- Tree.py
+++ Tree.py
@@ -1,62 +1,33 @@
 # 13.02.2025
 
 
+import weakref
+
 class Tree:
 	def __init__(self, data):
 		self.data = data
-		self.children = []
-		self.parent = None
+		self.children = {}  # Use a dictionary for fast lookups
+		self.parent = None  # Weak reference can be used
+
+	def __repr__(self):
+		return f"Tree({self.data})"
 
 	def is_root(self):
-		return self.parent == None
+		return self.parent is None
 
 	def add_edge(self, data):
-		aid = Tree(data)
-		aid.parent = self
-		self.children.append(aid)
+		"""Adds a child node only if it doesn't already exist."""
+		if data not in self.children:
+			child = Tree(data)
+			child.parent = weakref.ref(self)  # Use a weak reference
+			self.children[data] = child
+		return self.children[data]  # Return child for chaining
 
 	def search_child(self, data):
-		for node in self.children:
-			if node.data == data:
-				return node
-		return None
+		"""O(1) lookup for a child."""
+		return self.children.get(data, None)
 
-# Sells. Shells. Shore.
-
-root = Tree("s")
-
-root.add_edge("h")
-root.add_edge("e")
-
-root.search_child("h").add_edge("e")
-root.search_child("h").add_edge("o")
-root.search_child("e").add_edge("l")
-root.search_child("e").add_edge("a")
-
-root.search_child("h").search_child("e").add_edge(".")
-root.search_child("e").search_child("l").add_edge("l")
-root.search_child("e").search_child("a").add_edge(".")
-root.search_child("h").search_child("e").add_edge("l")
-root.search_child("h").search_child("o").add_edge("r")
-
-root.search_child("e").search_child("l").search_child("l").add_edge("s")
-root.search_child("h").search_child("e").search_child("l").add_edge("l")
-root.search_child("h").search_child("o").search_child("r").add_edge("e")
-
-root.search_child("e").search_child("l").search_child("l").search_child("s").add_edge(".")
-root.search_child("h").search_child("e").search_child("l").search_child("l").add_edge("s")
-root.search_child("h").search_child("o").search_child("r").search_child("e").add_edge(".")
-
-root.search_child("h").search_child("e").search_child("l").search_child("l").search_child("s").add_edge(".")
-
-def aid(node):
-	if node.data == ".":
-		print()
-	else:
-		for arg in node.children:
-			if node.is_root:
-				print(node.data, end="")
-			print(arg.data, end="")
-			aid(arg)
-
-aid(root)
+	def remove_child(self, data):
+		"""Removes a child node if it exists."""
+		if data in self.children:
+			del self.children[data]  # Remove from dictionary
